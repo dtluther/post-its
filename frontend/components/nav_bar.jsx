438e44f40504b3b93c26de96a358938c6713ba63
Dillon <dillontluther@gmail.com>
2018-01-28 15:30:00 -0800

    new note styled

--- frontend/components/nav_bar.jsx
+++ frontend/components/nav_bar.jsx
@@ -1,29 +1,71 @@
 import React from 'react';
-// import Modal from 'react-modal';
+import Modal from 'react-modal';
+import NewNoteFormContainer from './note_form/new_note_form_container';
 
 class NavBar extends React.Component {
     constructor(props) {
         super(props);
 
-        // this.state = {
-        //     newNoteModalIsOpen: false
-        // };
+        this.state = {
+            noteFormModalIsOpen: false
+        };
 
-        // this.handleOpenNewNoteModal = this.handleOpenNewNoteModal.bind(this);
-        // this.handleCloseModal = this.handleOpenNewNoteModal.bind(this);
+        this.handleOpenNoteFormModal = this.handleOpenNoteFormModal.bind(this);
+        this.handleCloseModal = this.handleCloseModal.bind(this);
     }
 
-    // addNote(event) {
-    //     return event => {
-    //         event.preventDefault();
+    handleOpenNoteFormModal() {
+        this.handleCloseModal();
+        this.setState({noteFormModalIsOpen: true});
+    }
 
-    //     }
-    // }
+    handleCloseModal() {
+        this.setState({ noteFormModalIsOpen: false});
+    }
+    
+    newNoteModal() {
+        return (
+            <div className='nav-bar'>
+                <button className='add-note-btn' onClick={this.handleOpenNoteFormModal}>+  Add Note</button>
+                <Modal className="new-note-modal note-form-modal"
+                       overlayClassName="new-note-overlay note-form-overlay"
+                       isOpen={this.state.noteFormModalIsOpen}
+                       onAfterOpen={this.afterOpenLoginModal}
+                       onRequestClose={this.handleCloseModal}
+                       contentLabel="new-note-modal"
+                       style={{
+                           overlay: {
+                               position: 'fixed',
+                               top: 0,
+                               left: 0,
+                               right: 0,
+                               bottom: 0,
+                               backgroundColor: 'rgba(165, 165, 165, 0.3)'
+                           },
+                           content: {
+                               outline: 'none',
+                               backgroundColor: 'white',
+                               position: 'absolute',
+                               top: '50%',
+                               left: '50%',
+                               transform: 'translate(-50%, -50%)',
+                               borderRadius: '5px'
+                           }
+                       }}
+                >
+                    <NewNoteFormContainer
+                        welcomeState={this.state}
+                        handleCloseModals={this.handleCloseModal}
+                    />
+                </Modal>
+            </div>
+        )
+    }
 
     render() {
         return (
-            <div className='nav-bar'>
-                <button className='add-note-btn'>+  Add Note</button>
+            <div>
+                {this.newNoteModal()}
             </div>
         );
     }
--- frontend/components/note_form/note_form.jsx
+++ frontend/components/note_form/note_form.jsx
@@ -1,50 +1,69 @@
 import React from 'react';
 
 class NoteForm extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        if (this.props.formType === "new") {
-            this.state = {
-                color: "red",
-                title: "",
-                body: ""
-
-            };
-
-        } else if (this.props.formType === "edit") {
-            this.state = {
-                id: this.props.id,
-                color: this.props.color,
-                title: this.props.title,
-                body: this.props.body
-            };
-        }
+    if (this.props.formType === "new") {
+      this.state = {
+        color: "red",
+        title: "",
+        body: ""
+      };
+    } else if (this.props.formType === "edit") {
+      this.state = {
+        id: this.props.id,
+        color: this.props.color,
+        title: this.props.title,
+        body: this.props.body
+      };
     }
+  }
 
-    render() {
-        const submit = this.props.formType === "new" ? this.handleSubmit : this.handleSave;
-        return (
-            <div className='note-form'>
-                <div className='colors'>
-                    <div className='color red'></div>
-                    <div className='color green'></div>
-                    <div className='color yellow'></div>
-                    <div className='color blue'></div>
-                </div>
-                <input type="text"
-                       className='title'
-                       onChange={this.update('title')}
-                       placeholder="Enter title here..."
-                />
-                <input type="text"
-                       className='body'
-                       onChange={this.update('body')}
-                       placeholder="My note..."
-                />
+  update(field) {
+    return e => {
+      e.preventDefault();
+      this.setState({ [field]: e.target.value });
+    };
+  }
+
+  render() {
+    const submit =
+      this.props.formType === "new" ? this.handleSubmit : this.handleSave;
+    return (
+      <div className="note-form">
+        <div className="note-color">
+        </div>
+        <div className="note-info">
+            <div className="colors">
+                <div className="color red" />
+                <div className="color green" />
+                <div className="color yellow" />
+                <div className="color blue" />
             </div>
-        );
-    }
+            <input
+                type="text"
+                className="title"
+                onChange={this.update("title")}
+                placeholder="Untitled"
+            />
+            <textarea
+                className="body"
+                onChange={this.update("body")}
+                placeholder="Just start typing here"
+                style={{border: 0, outline: 0}}
+                rows="25"
+            />
+        </div>
+        <div className='note-btns'>
+            <button className='cancel note-btn'>Cancel</button>
+            <button className='submit note-btn'>Submit</button>
+        </div>
+      </div>
+    );
+  }
+
+
 }
 
 export default NoteForm;
