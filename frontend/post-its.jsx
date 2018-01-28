567e7f4d7adb04fb8b444604b309c33bdca1513d
Dillon <dillontluther@gmail.com>
2018-01-27 23:36:08 -0800

    forgot about views, updating those

--- frontend/post-its.jsx
+++ frontend/post-its.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// TESTING START
+import { createNote } from './util/note_api_util';
+
+window.createNote = createNote;
+// TESTING END
+
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Welcome to Post-Its!</h1>, root);
