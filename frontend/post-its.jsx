a1756b1743dac31b9cf7e5aaf3d83537a77b9654
Dillon <dillontluther@gmail.com>
2018-01-28 01:21:25 -0800

    added <Root/> and store to entry file

--- frontend/post-its.jsx
+++ frontend/post-its.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Root from './components/root';
+import configureStore from './store/store';
 
 // TESTING START
 import { createNote } from './util/note_api_util';
-
 window.createNote = createNote;
+
+
 // TESTING END
 
 document.addEventListener('DOMContentLoaded', () => {
+    const store = configureStore();
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>Welcome to Post-Its!</h1>, root);
+    ReactDOM.render(<Root store={store}/>, root);
 });
